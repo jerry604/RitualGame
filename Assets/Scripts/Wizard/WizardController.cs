596fc3a73625e1c5e9701aacc75d265a49ea9ac9
Jerry Tsai <jerry604@gmail.com>
2016-01-30 10:57:29 -0800

    woops

---
+++ Assets/Scripts/Wizard/WizardController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardController : MonoBehaviour {
+
+
+	float moveHorizontal = Input.GetAxis("Horizontal");
+	float moveVertical = Input.GetAxis("Vertical");
+
+	void FixedUpdate()
+	{
+		Input	
+	}
+}
