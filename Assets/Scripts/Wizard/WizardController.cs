3ff8e13b980870fa763b93bf0bf044bcd861fc54
Jerry Tsai <jerry604@gmail.com>
2016-01-30 13:33:38 -0800

    jumping

--- Assets/Scripts/Wizard/WizardController.cs
+++ Assets/Scripts/Wizard/WizardController.cs
@@ -3,12 +3,47 @@ using System.Collections;
 
 public class WizardController : MonoBehaviour {
 
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = true;
 
-	float moveHorizontal = Input.GetAxis("Horizontal");
-	float moveVertical = Input.GetAxis("Vertical");
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
 
-	void FixedUpdate()
+	private bool grounded = false;
+	private Rigidbody2D rb2d;
+
+	void Awake ()
 	{
-		Input	
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		if (h * rb2d.velocity.x < maxSpeed) {
+			rb2d.AddForce (Vector2.right * h * moveForce);
+		} 
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+		}
+
+		if (jump) {
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+
 	}
 }
