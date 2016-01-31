dddbaa5910000f886e5afae77c3c0c923707f1f5
Jerry Tsai <jerry604@gmail.com>
2016-01-31 12:52:16 -0800

    level set up in kevin.scene

--- Assets/Scripts/cameraFollowing.cs
+++ Assets/Scripts/cameraFollowing.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class cameraFollowing : MonoBehaviour
 {
-    public WizardController target;
-    public float smooth = 3.0f;
-    void Update()
-    {
-        Vector3 newPos = new Vector3(System.Math.Min(System.Math.Max(target.wizardX, target.minCameraX), target.maxCameraX), transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(
-            transform.position, newPos,
-            Time.deltaTime * smooth);
-    }
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
 
 }
