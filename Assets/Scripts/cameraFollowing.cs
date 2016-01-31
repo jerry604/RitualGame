e1870919a61b68cacecd4d2c2887e00541745bfd
Kevin Jayamanne <Kevin Jayamanne>
2016-01-30 19:51:51 -0800

    Respliced walk animations to be less choppy, added jump animation (kinda), set camera boundaries

--- Assets/Scripts/cameraFollowing.cs
+++ Assets/Scripts/cameraFollowing.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class cameraFollowing : MonoBehaviour
 {
-    public Transform target;
+    public WizardController target;
     public float smooth = 3.0f;
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        Vector3 newPos = new Vector3(System.Math.Min(System.Math.Max(target.wizardX, target.minCameraX), target.maxCameraX), transform.position.y, transform.position.z);
         print(newPos);
         transform.position = Vector3.Lerp(
             transform.position, newPos,
             Time.deltaTime * smooth);
+        print(newPos);
     }
 
 }
