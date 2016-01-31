cbe36c0ec396d8720d558a218b6b3f23385a7c03
Kevin Jayamanne <Kevin Jayamanne>
2016-01-30 18:55:13 -0800

    Added in Jen's background, added camera following script, and increased jump power a bit

---
+++ Assets/Scripts/cameraFollowing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollowing : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 3.0f;
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        print(newPos);
+        transform.position = Vector3.Lerp(
+            transform.position, newPos,
+            Time.deltaTime * smooth);
+    }
+
+}
