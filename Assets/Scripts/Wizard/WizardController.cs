bee054b6d9879bcb9066462c95b20b55fa31df86
Kevin Jayamanne <Kevin Jayamanne>
2016-01-30 16:24:02 -0800

    changes to player movement mechanics, implemented animation movement and idle states

--- Assets/Scripts/Wizard/WizardController.cs
+++ Assets/Scripts/Wizard/WizardController.cs
@@ -6,17 +6,19 @@ public class WizardController : MonoBehaviour {
 	[HideInInspector] public bool facingRight = true;
 	[HideInInspector] public bool jump = true;
 
-	public float moveForce = 365f;
-	public float maxSpeed = 5f;
+	public float moveForce = 30f;
+	public float maxSpeed = 10f;
 	public float jumpForce = 1000f;
 	public Transform groundCheck;
 
 	private bool grounded = false;
 	private Rigidbody2D rb2d;
+    private Animator anim;
 
-	void Awake ()
-	{
-		rb2d = GetComponent<Rigidbody2D> ();
+    void Awake ()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update ()
@@ -32,7 +34,20 @@ public class WizardController : MonoBehaviour {
 	{
 		float h = Input.GetAxis ("Horizontal");
 
-		if (h * rb2d.velocity.x < maxSpeed) {
+        if (Input.GetButton("Horizontal"))
+        {
+            if (h > 0)
+            {
+                anim.Play("walkRight");
+            }
+            if( h < 0 )
+            {
+                anim.Play("walkLeft");
+            }
+        }
+        
+
+        if (h * rb2d.velocity.x < maxSpeed) {
 			rb2d.AddForce (Vector2.right * h * moveForce);
 		} 
 
@@ -42,8 +57,27 @@ public class WizardController : MonoBehaviour {
 
 		if (jump) {
 			rb2d.AddForce(new Vector2(0f, jumpForce));
-			jump = false;
+            /* Apply motion */
+            //Vector2 _velocity = rb2d.velocity;
+            //Vector2 target = new Vector2(0, 5);
+            //Vector2 _velocityChange = ( target - _velocity);
+
+            /* Move rigidbody */
+            //rb2d.AddForce(_velocityChange, ForceMode.VelocityChange);
+            jump = false;
 		}
+
+        if( !Input.GetButton("Horizontal"))
+        {
+            if (grounded)
+            {
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            }
+            if ( rb2d.velocity.y == 0 )
+            {
+                anim.Play("idle");
+            }
+        }
 
 	}
 }
