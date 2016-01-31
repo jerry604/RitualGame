4f96f609842c52a728c830c2ddb24dc604c3317c
Jerry Tsai <jerry604@gmail.com>
2016-01-30 17:51:01 -0800

    shooting flame + destoryer logic

---
+++ Assets/Scripts/ShootingFlameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingFlameController : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		rb2d.velocity = new Vector2 (0, speed);
+	}
+}
